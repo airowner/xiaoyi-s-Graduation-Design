3526b49144ac3c789c619813c82979fe8a74f2d2
zhanghua <zhanghua@360.cn>
2012-06-08 19:22:15 +0800

    fix

--- index.py
+++ index.py
@@ -3,23 +3,80 @@ import web
 
 urls = (
     '/', 'index', # 首页
-    '/', 'myphoto', # 我的相薄
-    '/', 'message', # 给我留言
-    '/', 'case', #所学课程
-    '/', 'mypower', #个人能力
-    '/', 'myprofile', #个人简介
-    '/', 'jobtarget', #求职意向
+    '/myphoto', 'myphoto', # 我的相薄
+    '/message', 'message', # 给我留言
+    '/case', 'case', #所学课程
+    '/mypower', 'mypower', #个人能力
+    '/myprofile', 'myprofile', #个人简介
+    '/jobtarget', 'jobtarget', #求职意向
     )
 
 app = web.application(urls, globals())
-render = web.template.render('template', base='layout.html')
+
+t_globals = {
+        '_SERVER': web.ctx,
+        'nav': [ '/', '/myphoto', '/message', '/case', '/mypower', '/myprofile', '/jobtarget' ],
+        'nav_name': [ '首页', '我的相薄', '给我留言', '所学课程', '个人能力', '个人简介', '求职意向' ],
+        }
+render = web.template.render('template', globals=t_globals)
 
 
 class index:
     def GET(self):
-        return render.index()
+        info = {}
+        info['title'] = '首页'
+        left = render.style_main()
+        right = render.style_right()
+        return render.layout(info, left, right)
 
+class myphoto:
+    def GET(self):
+        info = {}
+        info['title'] = '我的相薄'
+        left = render.index() 
+        right = ''
+        return render.layout(info, left, right)
 
+class message:
+    def GET(self):
+        info = {}
+        info['title'] = '给我留言'
+        left = ''
+        right = ''
+        return render.layout(info, left, right)
+
+class case:
+    def GET(self):
+        info = {}
+        info['title'] = '所学课程'
+        left = ''
+        right = ''
+        return render.layout(info, left, right)
+
+class mypower:
+    def GET(self):
+        info = {}
+        info['title'] = '个人能力'
+        left = ''
+        right = ''
+        return render.layout(info, left, right)
+
+class myprofile:
+    def GET(self):
+        info = {}
+        info['title'] = '个人简介'
+        left = ''
+        right = ''
+        return render.layout(info, left, right)
+
+class jobtarget:
+    def GET(self):
+        info = {}
+        info['title'] = '求职意向'
+        left = ''
+        right = ''
+        return render.layout(info, left, right)
 
 if __name__ == '__main__':
+    web.application.internalerror = web.debugerror
     app.run()
