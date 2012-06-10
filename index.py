ff7edf378b9b4ade61deee57d6f08a68663b55d7
airowner <airowner@gmail.com>
2012-06-11 02:11:31 +0800

    add database

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
-# _*_ coding: utf-8 _*_
-import web
+﻿# _*_ coding: utf-8 _*_
+import web, xy_db
+
+import datetime
+
 
 urls = (
     '/', 'index', # 首页
     '/myphoto', 'myphoto', # 我的相薄
-    '/message', 'message', # 给我留言
+    '/message(/.*)?', 'message', # 给我留言
     '/case', 'case', #所学课程
     '/mypower', 'mypower', #个人能力
     '/myprofile', 'myprofile', #个人简介
@@ -12,71 +15,102 @@ urls = (
     )
 
 app = web.application(urls, globals())
+now = datetime.datetime.utcnow()
+
+def Timestringify(timestamp):
+    return web.datestr(datetime.datetime.utcfromtimestamp(timestamp), now=now)
 
 t_globals = {
         '_SERVER': web.ctx,
         'nav': [ '/', '/myphoto', '/message', '/case', '/mypower', '/myprofile', '/jobtarget' ],
         'nav_name': [ '首页', '我的相薄', '给我留言', '所学课程', '个人能力', '个人简介', '求职意向' ],
+        'timestringify': Timestringify,
+        'now': now,
         }
+
 render = web.template.render('template', globals=t_globals)
+_db = xy_db.XYDB()
 
+class base:
+    def __init__(self):
+        self.msg = web.input(msg='')
+        
+    def sidebar(self):
+        return unicode(render.right(_db.getRight()))
 
-class index:
+class index(base):
     def GET(self):
         info = {}
         info['title'] = '首页'
         left = render.style_main()
-        right = render.style_right()
-        return render.layout(info, left, right)
+        return render.layout(info, left, self.sidebar())
 
-class myphoto:
+class myphoto(base):
     def GET(self):
         info = {}
         info['title'] = '我的相薄'
-        left = render.index() 
-        right = ''
-        return render.layout(info, left, right)
+        left = render.style_main() 
+        return render.layout(info, left, self.sidebar())
 
-class message:
-    def GET(self):
+class message(base):
+    def GET(self, tid=None):
         info = {}
         info['title'] = '给我留言'
-        left = ''
-        right = ''
-        return render.layout(info, left, right)
+        msgs = {}
+        reply = []
+        if tid:
+            tid = tid[1:]
+            msgs = list(_db.getLeavemsg(where='id='+web.sqlquote(tid)))[0]
+            reply = _db.getLeavemsg(where='parent='+web.sqlquote(tid), order='time asc')
+        left = unicode(render.message(msgs, list(reply)))
+        return render.layout(info, left, self.sidebar(), self.msg)
 
-class case:
+    def POST(self, tid=None):
+        data = web.input()
+        for k, v in web.ctx.iteritems():
+            if k == 'environ':
+                refer = v['HTTP_REFERER']
+                break
+        if not refer:
+            refer = '/'
+            
+        if not data.name or not data.email or not data.message:      
+            return web.seeother(refer + '?msg=评论失败')
+        parent = data.get('id', '')
+        _db.addLeavemsg(data.name, data.email, data.message, parent=parent, message=data.rows)
+        return web.seeother(refer + '?msg=评论成功')
+
+class case(base):
     def GET(self):
         info = {}
         info['title'] = '所学课程'
-        left = ''
-        right = ''
-        return render.layout(info, left, right)
+        left = render.archives_main()
+        return render.layout(info, left, self.sidebar())
 
-class mypower:
+class mypower(base):
     def GET(self):
         info = {}
         info['title'] = '个人能力'
         left = ''
-        right = ''
-        return render.layout(info, left, right)
+        return render.layout(info, left, self.sidebar())
 
-class myprofile:
+class myprofile(base):
     def GET(self):
         info = {}
         info['title'] = '个人简介'
         left = ''
-        right = ''
-        return render.layout(info, left, right)
+        return render.layout(info, left, self.sidebar())
 
 class jobtarget:
     def GET(self):
         info = {}
         info['title'] = '求职意向'
         left = ''
-        right = ''
-        return render.layout(info, left, right)
+        return render.layout(info, left, self.sidebar())
 
 if __name__ == '__main__':
-    web.application.internalerror = web.debugerror
+    import os, sys
+    os.chdir(sys.path[0])
+    
+    #web.application.internalerror = web.debugerror
     app.run()
---
+++ xy_db.py
@@ -0,0 +1,140 @@
+﻿# coding=utf-8
+
+import os, web, time, hashlib
+
+def toUnicode(text):
+    if type(text).__name__ == 'unicode':
+        return text
+    elif type(text).__name__ == 'str':
+        return unicode(text, 'utf-8')
+    else:
+        return unicode(str(text), 'utf-8')
+    
+class ExistsRecordError(Exception): pass
+
+class XYDB:
+    def __init__(self):
+
+        dbfile = os.path.realpath(os.curdir) + '/test.db'
+        self.db = web.database(dbn="sqlite", db=dbfile)
+        self.db.supports_multiple_insert = True
+        
+        if not os.path.exists(dbfile):
+            """
+            category 分类
+            parent   第一次发表为空，如果是回复，则这里是原消息的id字段
+            user     用户名
+            email    用户email
+            title    标题
+            time     添加时间戳
+            message  内容
+            id       哈希， md5(category + user + title), 限制: 同一个用户再同一分类下不能发表目录名一致的文章
+            """
+            self.db.query("create table topic ( category text, parent text, user text, email text, title text, time real, message text, id text )")
+
+            #留言板
+            self.addLeavemsg("zhanghua", "airowner@gmail.com", "你好，很高兴认识你丫!")
+            self.addLeavemsg("陈怡", "295387148@qq.com", "你好，认识你也很高兴", parent="d4eafca37aa5908a497dfd527a72bb2a", message="d4eafca37aa5908a497dfd527a72bb2a")
+            self.addLeavemsg("zhanghua", "airowner@gmail.com", "QQ是多少啊？可以加个好友吗？", parent="d4eafca37aa5908a497dfd527a72bb2a", message="db382e936792b162f0bc429f312456b9")
+            self.addLeavemsg("yanyan", "enenqin@gmail.com", "好久不见，最近怎么样啊!")
+
+            #课程
+            self.addCase("陈怡", "295387148@qq.com", "语文", message="2012-02-07~2012-02-08 语文语文")
+            self.addCase("陈怡", "295387148@qq.com", "数学", message="2012-02-07~2012-02-08 数学数学")
+            self.addCase("陈怡", "295387148@qq.com", "英语", message="2012-02-07~2012-02-08 英语英语")
+            self.addCase("陈怡", "295387148@qq.com", "自习", message="2012-02-07~2012-02-08 自习自习")
+            self.addCase("陈怡", "295387148@qq.com", "地理", message="2012-02-07~2012-02-08 地理地理")
+
+            #相册
+            self.addAlbum("陈怡", "295387148@qq.com", "我的风采")
+            self.addAlbum("陈怡", "295387148@qq.com", "校园风光")
+
+
+    def commonParam(self, category, user, email, title, **keys):
+        ret = {}
+        ret['category'] = toUnicode(category)
+        ret['user'] = toUnicode(user)
+        ret['email'] = toUnicode(email)
+        ret['title'] = toUnicode(title)
+        ret['id'] = XYDB.getID(category, user, title)
+        if len(list(self.db.select('topic', where='category='+web.sqlquote(category)+' and id='+web.sqlquote(ret['id'])))):
+            raise ExistsRecordError('record is exists')
+        ret['time'] = int(time.time())
+        ret['parent'] = ''
+        ret['message'] = ''
+        for k, v in keys.items():
+            ret[k] = toUnicode(v)
+        return ret
+
+    #留言板
+    def addLeavemsg(self, user, email, title, **keys):
+        return self.db.multiple_insert('topic', [self.commonParam('leavemsg', user, email, title, **keys)])
+    
+    #课程
+    def addCase(self, user, email, title, **keys):
+        return self.db.multiple_insert('topic', [self.commonParam('case', user, email, title, **keys)])
+
+    #相册
+    def addAlbum(self, user, email, title, **keys):
+        return self.db.multiple_insert('topic', [self.commonParam('album', user, email, title, **keys)])
+
+    #个人能力
+    def addPower(self, user, email, title, **keys):
+        return self.db.multiple_insert('topic', [self.commonParam('power', user, email, title, **keys)])
+
+    #个人简介
+    #def addProfiles(self, user, email, title, **keys):
+     #   return self.db.multiple_insert('topic', [self.commonParam('profile', user, email, title, **keys)])
+
+    #求职意向
+    def addJob(self, user, email, title, **keys):
+        return self.db.multiple_insert('topic', [self.commonParam('job', user, email, title, **keys)])
+
+
+    @staticmethod
+    def getID(category, user, title):
+        print(category)
+        print(user)
+        print(title)
+        return hashlib.md5(category + user + title).hexdigest()
+
+    def getCommon(self, category, **kws):
+        where = "category=" + web.sqlquote(category)
+        if kws.has_key("where"):
+            where += " and " + kws["where"]
+            del kws["where"]
+        if kws.has_key("order"):
+            order = kws["order"]
+            del kws["order"]
+        else:
+            order = "time desc"
+            
+        return self.db.select("topic", where=where, order=order, **kws)
+
+    def getLeavemsg(self, **kws):
+        return self.getCommon("leavemsg", **kws)
+    
+    def getRight(self):
+        return self.getLeavemsg(limit="5")
+
+
+if __name__ == "__main__":
+    import sys
+    os.chdir(sys.path[0])
+    print('curpath:' + sys.path[0]+"\n\n")
+    if os.path.exists(os.path.realpath(os.curdir) + '/test.db'):
+        os.unlink(os.path.realpath(os.curdir) + '/test.db')
+
+    db = XYDB()
+    print(list(db.db.select('topic')))
+
+    for i in db.db.select("topic").list():
+        print i
+    msgs = db.getCommon('leavemsg', where="user="+web.sqlquote('zhanghua') )
+    print msgs
+    for i in msgs.list():
+        print i.user
+
+    print db.addLeavemsg("zhanghua", "airowner@gmail.com", "你好，很高兴认识你丫!")
+    print db.addLeavemsg("陈怡", "295387148@qq.com", "你好，认识你也很高兴")
+    
